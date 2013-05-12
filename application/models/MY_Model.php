44cd1d0ea97120e190cffbabf554b00a4a3acb16
Kell-Tainer <kell.tainer.13@gmail.com>
2013-05-12 15:29:15 +0200

    Premier Commit du projet

---
+++ application/models/MY_Model.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+// -----------------------------------------------------------------------------
+ 
+class MY_Model extends CI_Model
+{
+    /**
+     *  Insère une nouvelle ligne dans la base de données.
+     */
+    public function create()
+    {
+         
+    }
+ 
+    /**
+     *  Récupère des données dans la base de données.
+     */
+    public function select()
+    {
+         
+    }
+     
+    /**
+     *  Modifie une ou plusieurs lignes dans la base de données.
+     */
+    public function update()
+    {       
+         
+    }
+     
+    /**
+     *  Supprime une ou plusieurs lignes de la base de données.
+     */
+    public function delete()
+    {
+         
+    }
+ 
+    /**
+     *  Retourne le nombre de résultats.
+     */
+    public function count()
+    {
+         
+    }
+}
+ 
+/* End of file MY_Model.php */
+/* Location: ./system/application/core/MY_Model.php */
+?>
